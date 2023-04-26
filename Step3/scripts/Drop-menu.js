636eac6483e3e95ac7c6639f95b1ec05fb328630
Fulgrim <ivanyuschin.work@yandex.ru>
2023-04-26 20:47:09 +0300

    рефакторинг главного меню заершенЭ

--- Step3/scripts/Drop-menu.js
+++ Step3/scripts/Drop-menu.js
@@ -1,13 +1,13 @@
 /* Когда пользователь нажимает на кнопку,
 переключение между скрытием и отображением раскрывающегося содержимого */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("menu-links").classList.toggle("show");
   }
   
   // Закройте выпадающее меню, если пользователь щелкает за его пределами
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+    if (!event.target.matches('.header__menu-drop-btn')) {
+      var dropdowns = document.getElementsByClassName("header__menu-links");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
